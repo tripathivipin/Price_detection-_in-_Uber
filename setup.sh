d83f661918ee0e796055cdf4cb4830d43f975290
tripathivipin <106297012+tripathivipin@users.noreply.github.com>
2022-10-05 22:15:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"mahalelalit45@gmail.com\"\n\
+email = \"tripathivipin078@gmail.com\"\n\
 "> ~/.stramlit/credentials.toml
 echo "\
 [server]\n\
